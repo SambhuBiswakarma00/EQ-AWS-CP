4dc355bd1c13e88f366c7f6ed802af82eb2b2287
SambhuBiswakarma00 <biswakarmasambhu@gmail.com>
2024-04-08 19:47:38 +0530

    commenting rds and dynamodb section for test

--- app.py
+++ app.py
@@ -6,11 +6,11 @@ from config import *
 app = Flask(__name__)
 
 # MySQL configurations
-RDS_HOST = 'your_rds_host'
-RDS_PORT = 3306
-RDS_USER = 'your_rds_user'
-RDS_PASSWORD = 'your_rds_password'
-RDS_DB_NAME = 'your_rds_db_name'
+# RDS_HOST = 'your_rds_host'
+# RDS_PORT = 3306
+# RDS_USER = 'your_rds_user'
+# RDS_PASSWORD = 'your_rds_password'
+# RDS_DB_NAME = 'your_rds_db_name'
 
 # Initialize Boto3 DynamoDB client
 dynamodb = boto3.client('dynamodb')
@@ -59,7 +59,7 @@ def hello_world():
 
         # Upload file to S3
         s3 = boto3.resource('s3')
-        s3 = boto3.resource(service_name='s3', region_name='us-east-1', aws_access_key_id=key_id, aws_secret_access_key=access_key)
+        # s3 = boto3.resource(service_name='s3', region_name='us-east-1', aws_access_key_id=key_id, aws_secret_access_key=access_key)
         file_body = request.files['file_name']
         file_name = file_body.filename
         count_obj = 0
@@ -73,16 +73,16 @@ def hello_world():
         except Exception as e:
             return str(e)
 
-        try:
-            # Upload data to RDS
-            cursor = db_conn.cursor()
-            insert_sql = "INSERT INTO intellipaattab VALUES (%s)"
-            cursor.execute(insert_sql, (file_name))
-            db_conn.commit()
-            print("Data uploaded to RDS successfully !")
-        except Exception as e:
-            print(str(e))
-            return str(e)
+        # try:
+        #     # Upload data to RDS
+        #     cursor = db_conn.cursor()
+        #     insert_sql = "INSERT INTO intellipaattab VALUES (%s,%s,%s,%s)"
+        #     cursor.execute(insert_sql, (name, location, age, technology))
+        #     db_conn.commit()
+        #     print("Data uploaded to RDS successfully !")
+        # except Exception as e:
+        #     print(str(e))
+        #     return str(e)
 
         try:
             # Upload user data to DynamoDB
@@ -100,18 +100,18 @@ def hello_world():
 
     return render_template("index.html")
 
-@app.route("/employee", methods=['GET', 'POST'])
-def get_employee():
-    if request.method == 'POST':
-        employee_id = request.form['employee_id']
-        employee_data = query_employee_data(employee_id)
+# @app.route("/employee", methods=['GET', 'POST'])
+# def get_employee():
+#     if request.method == 'POST':
+#         employee_id = request.form['employee_id']
+#         employee_data = query_employee_data(employee_id)
 
-        if employee_data:
-            return render_template('employee_details.html', employee=employee_data)
-        else:
-            return render_template('error.html', message="Employee not found")
+#         if employee_data:
+#             return render_template('employee_details.html', employee=employee_data)
+#         else:
+#             return render_template('error.html', message="Employee not found")
 
-    return render_template('employee_form.html')
+#     return render_template('employee_form.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
